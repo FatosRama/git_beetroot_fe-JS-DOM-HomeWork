14c1b4574bf21bd2f939de36202c3bbfd50a45a9
Fatos Rama <fatosrama.fr@gmail.com>
2025-11-28 16:42:01 +0100

    Finished the first assignment

---
+++ app.js
@@ -0,0 +1,43 @@
+var playList = [
+{
+ author: "LED ZEPPELIN",
+ song:"STAIRWAY TO HEAVEN"
+},
+{
+ author: "QUEEN",
+ song:"BOHEMIAN RHAPSODY"
+},
+{
+ author: "LYNYRD SKYNYRD",
+ song:"FREE BIRD"
+},
+{
+ author: "DEEP PURPLE",
+ song:"SMOKE ON THE WATER"
+},
+{
+ author: "JIMI HENDRIX",
+ song:"ALL ALONG THE WATCHTOWER"
+},
+{
+ author: "AC/DC",
+ song:"BACK IN BLACK"
+},
+{
+ author: "QUEEN",
+ song:"WE WILL ROCK YOU"
+},
+{
+ author: "METALLICA",
+ song:"ENTER SANDMAN"
+}
+];
+
+
+let numberedList = document.getElementById("rock_playlist");
+for(let key1 of Object.entries(playList)){
+    console.log(key1[1].author);
+    let li = document.createElement("li");
+    li.innerHTML=`Author: ${key1[1].author} - Song: ${key1[1].song}`;
+    numberedList.appendChild(li);
+}
